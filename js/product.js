41fed3293600ba170e0dcd3e6ce2ab5f6df227ed
‘Michael <pickup.patterns@gmail.com>
2018-11-07 13:10:39 -0800

    switching to a new branch

--- js/product.js
+++ js/product.js
@@ -1,4 +1,6 @@
 //this is just your data into an array to be randominzed
+//be sure to load it into local storage
+//chart from chart js on the reporting page container has to have a width or else you wont see it  
 
 export default [
     {
@@ -26,16 +28,56 @@ export default [
         // views:
     },
     {
-        name: 'boots',
+        name: 'breakfast',
         image: '../assets/breakfast.jpg',
         // clicks:
         // views:
     },
     {
-        name: 'boots',
+        name: 'bubblegum',
         image: '../assets/bubblegum.jpg',
         // clicks:
         // views:
+    },
+    
+    {
+        name: 'chair',
+        image: '../assets/chair.jpg',
+        // clicks:
+        // views:
+    },
+    {
+        name: 'cthulhu',
+        image: '../assets/cthulhu.jpg',
+        // clicks:
+        // views:
+    },
+    {
+        name: 'dog-duck',
+        image: '../assets/dog-duck.jpg',
+        // clicks:
+        // vie
+    },
+    {
+        name: 'dragon',
+        image: '../assets/dragon.jpg',
+        // clicks:
+        // vie
+    },
+    {
+        name: 'pen',
+        image: '../assets/pen.jpg',
+        // clicks:
+        // vie
     }
-
 ];
+
+// // export default {
+// //     getAll() {
+// //         return WebGLShaderPrecisionFormat;
+
+// //     },
+// //     save(){
+// //         window.localStorage.setItems('pics, JSON.Stringify.picker')
+// //     }
+// }
